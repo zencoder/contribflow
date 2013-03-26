40b52e95fae9e1b0609eb0643797596831045e41
Steve Heffernan <steve@zencoder.com>
2013-03-25 17:16:52 -0700

    Added grunt tasks to own grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,6 +42,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
+  grunt.loadTasks('./tasks');
+
   // Default task.
   grunt.registerTask('default', ['jshint', 'nodeunit']);
 
